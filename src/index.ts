7cba23300b152c0db7dfd1beefb4db723112881b
tandica <williams.tandica@gmail.com>
2022-05-30 17:21:05 -0400

    add notes on functions and objects

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,17 @@ let level;
 
 //for arrays in typescript, if you want to set it to an empty array, you need to specify hat kind of elements will go into the array
 //arrays can only have one type of value
-let numbers: number [] = [];
+let numbers: number[] = [];
+
+//enum
+enum Size {
+  Small = 1,
+  Medium,
+  Large,
+}
+let mySize: Size = Size.Medium;
+console.log(mySize);
+//output will be 2, because small is equal to 1 and 2 is the next number
+
+//objects
+let employee: { id: number; name: string } = { id: 1, name: "Bob" };
