17bfabe0165ffacd8a9cf5dc813ba2485e40b218
tandica <williams.tandica@gmail.com>
2022-05-30 09:20:39 -0400

    add notes for arrays

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ let sales = 123_456_789;
 let course = "Typescript";
 let is_published = true;
 let level;
+
+//for arrays in typescript, if you want to set it to an empty array, you need to specify hat kind of elements will go into the array
+//arrays can only have one type of value
+let numbers: number [] = [];
